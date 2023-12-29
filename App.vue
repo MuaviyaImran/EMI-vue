7de95fae835fcbf492ccafcabd9d03ece449ca67
Muaviya Imran <muaviyaimran1122@gmail.com>
2023-12-29 11:34:22 +0500

    Changed filenames as original for build

--- App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
-<template>
-  <div class="bg-black w-full">
-    <HelloWorld :fields="4" />
-  </div>
-</template>
