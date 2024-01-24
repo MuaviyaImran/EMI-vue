e4ac1dfeb2b146e26a9a924efe7fc7093ee6d8bf
Muaviya Imran <muaviyaimran1122@gmail.com>
2024-01-24 16:28:53 +0500

    Added Date Range Calender

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 // const formKitTailwind = require("@formkit/themes/tailwindcss");
+const colors = require("tailwindcss/colors");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/vue-tailwind-datepicker/**/*.js",
     // "./formkit-custom-theme.js",
   ],
   theme: {
@@ -19,12 +21,14 @@ export default {
           dark: "#4C4639",
           "semi-dark": "#5F5E5E",
         },
+        "vtd-primary": colors.yellow,
+        "vtd-secondary": colors.gray,
         tertiary: { DEFAULT: "#CFC6B4", light: "#FFF8F0" },
         success: { DEFAULT: "#38A457", light: "#F4FFFB" },
         warning: { DEFAULT: "#FB9F34", light: "#F9E6D0" },
         danger: { DEFAULT: "#B33A3A", light: "#F3DFDF" },
         info: { DEFAULT: "#2196F3", light: "#EBF4FF" },
-        "border-primary":{DEFAULT:"#e3e3e3"}
+        "border-primary": { DEFAULT: "#e3e3e3" },
       },
       boxShadow: {
         xs: "0px 0px 5px 0px rgba(166, 176, 184, 0.39)",
@@ -34,4 +38,5 @@ export default {
     },
   },
   // plugins: [formKitTailwind],
+  plugins: [require("@tailwindcss/forms")],
 };
